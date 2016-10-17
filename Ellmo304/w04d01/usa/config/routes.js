2430e7a7d5f57958734b739814c9e97709f806d3
Elliot Brock <ell_304@msn.com>
2016-10-17 22:26:32 +0100

    poll page added

--- Ellmo304/w04d01/usa/config/routes.js
+++ Ellmo304/w04d01/usa/config/routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.get("/", (req, res) => res.render("index"));
 router.get("/donald", (req, res) => res.render("donald"));
 router.get("/hilary", (req, res) => res.render("hilary"));
+router.get("/polls", (req, res) => res.render("polls"));
 
 
 module.exports = router;
