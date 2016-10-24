1514ab6faf1b5ee4e7a63ce84635bb804f0f5294
Elliot Brock <ell_304@msn.com>
2016-10-24 22:25:15 +0100

    added user current location button/marker/function

--- Ellmo304/w05d01/starter-code/public/js/app.js
+++ Ellmo304/w05d01/starter-code/public/js/app.js
@@ -1,55 +1,73 @@
 "use strict";
 
-// BONUS: Mark your current location on the map
 // BONUS BONUS: Add restul routes
 
+$(function () {
 
-var googleMap = googleMap || {};
+  var googleMap = googleMap || {};
 
-googleMap.getRestaurants = function () {
-  $.get("http://localhost:3000/api/restaurants").done(this.loopThroughRestaurants);
-};
-
-googleMap.addInfoWindow = function (restaurant, marker) {
-  var _this = this;
-
-  google.maps.event.addListener(marker, "click", function () {
-    if (_this.infowindow) {
-      _this.infowindow.close();
-    }
-    _this.infowindow = new google.maps.InfoWindow({
-      content: "<h1>" + restaurant.name + "</h1>\n      <img src=\"" + restaurant.image + "\"/>\n      <p>" + restaurant.description + "</p>"
-    });
-    _this.infowindow.open(_this.map, marker);
-  });
-};
-
-googleMap.mapSetup = function () {
-  var canvas = document.getElementById("map-canvas");
-
-  var mapOptions = {
-    zoom: 14,
-    center: new google.maps.LatLng(51.522, -0.089),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+  googleMap.getRestaurants = function () {
+    $.get("http://localhost:3000/api/restaurants").done(this.loopThroughRestaurants);
   };
-  this.map = new google.maps.Map(canvas, mapOptions);
-  this.getRestaurants();
-};
 
-googleMap.createMarkerForRestaurant = function (restaurant) {
-  var latLng = new google.maps.LatLng(restaurant.lat, restaurant.lng);
-  var marker = new google.maps.Marker({
-    position: latLng,
-    map: googleMap.map,
-    icon: "/images/marker.png"
+  googleMap.addInfoWindow = function (restaurant, marker) {
+    var _this = this;
+
+    google.maps.event.addListener(marker, "click", function () {
+      if (_this.infowindow) {
+        _this.infowindow.close();
+      }
+      _this.infowindow = new google.maps.InfoWindow({
+        content: "<h1>" + restaurant.name + "</h1>\n        <img src=\"" + restaurant.image + "\"/>\n        <p>" + restaurant.description + "</p>"
+      });
+      _this.infowindow.open(_this.map, marker);
+    });
+  };
+
+  googleMap.mapSetup = function () {
+    var canvas = document.getElementById("map-canvas");
+
+    var mapOptions = {
+      zoom: 14,
+      center: new google.maps.LatLng(51.522, -0.089),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(canvas, mapOptions);
+    this.getRestaurants();
+  };
+
+  googleMap.createMarkerForRestaurant = function (restaurant) {
+    var latLng = new google.maps.LatLng(restaurant.lat, restaurant.lng);
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: googleMap.map,
+      icon: "/images/marker.png"
+    });
+    googleMap.addInfoWindow(restaurant, marker);
+  };
+
+  googleMap.loopThroughRestaurants = function (data) {
+    $.each(data.restaurants, function (index, restaurant) {
+      googleMap.createMarkerForRestaurant(restaurant);
+    });
+  };
+
+  $(googleMap.mapSetup.bind(googleMap));
+
+  document.getElementById("locationButton").addEventListener("click", function () {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var latLng = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      googleMap.map.panTo(latLng);
+      googleMap.map.setZoom(14);
+      var marker = new google.maps.Marker({
+        position: latLng,
+        animation: google.maps.Animation.DROP,
+        draggable: true,
+        map: googleMap.map
+      });
+    });
   });
-  googleMap.addInfoWindow(restaurant, marker);
-};
-
-googleMap.loopThroughRestaurants = function (data) {
-  $.each(data.restaurants, function (index, restaurant) {
-    googleMap.createMarkerForRestaurant(restaurant);
-  });
-};
-
-$(googleMap.mapSetup.bind(googleMap));
+});
--- Ellmo304/w05d01/starter-code/src/js/app.js
+++ Ellmo304/w05d01/starter-code/src/js/app.js
@@ -1,64 +1,85 @@
 
-// BONUS: Mark your current location on the map
 // BONUS BONUS: Add restul routes
 
+$(() => {
+
+  var googleMap = googleMap || {};
 
 
-var googleMap = googleMap || {};
-
-
-googleMap.getRestaurants = function () {
-  $.get("http://localhost:3000/api/restaurants")
-  .done(this.loopThroughRestaurants);
-};
-
-
-
-googleMap.addInfoWindow = function (restaurant, marker) {
-  google.maps.event.addListener(marker, "click", () => {
-    if (this.infowindow) {
-      this.infowindow.close();
-    }
-    this.infowindow = new google.maps.InfoWindow({
-      content: `<h1>${restaurant.name}</h1>
-      <img src="${restaurant.image}"/>
-      <p>${restaurant.description}</p>`
-    });
-    this.infowindow.open(this.map, marker);
-  });
-};
-
-
-googleMap.mapSetup = function() {
-  let canvas = document.getElementById("map-canvas");
-
-  let mapOptions = {
-    zoom:14,
-    center: new google.maps.LatLng(51.522, -0.089),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+  googleMap.getRestaurants = function () {
+    $.get("http://localhost:3000/api/restaurants")
+    .done(this.loopThroughRestaurants);
   };
-  this.map = new google.maps.Map(canvas, mapOptions);
-  this.getRestaurants();
-
-};
 
 
-googleMap.createMarkerForRestaurant = (restaurant) => {
-  let latLng = new google.maps.LatLng(restaurant.lat, restaurant.lng);
-  let marker = new google.maps.Marker({
-    position: latLng,
-    map: googleMap.map,
-    icon: "/images/marker.png"
+
+  googleMap.addInfoWindow = function (restaurant, marker) {
+    google.maps.event.addListener(marker, "click", () => {
+      if (this.infowindow) {
+        this.infowindow.close();
+      }
+      this.infowindow = new google.maps.InfoWindow({
+        content: `<h1>${restaurant.name}</h1>
+        <img src="${restaurant.image}"/>
+        <p>${restaurant.description}</p>`
+      });
+      this.infowindow.open(this.map, marker);
+    });
+  };
+
+
+  googleMap.mapSetup = function() {
+    let canvas = document.getElementById("map-canvas");
+
+    let mapOptions = {
+      zoom:14,
+      center: new google.maps.LatLng(51.522, -0.089),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(canvas, mapOptions);
+    this.getRestaurants();
+
+  };
+
+
+  googleMap.createMarkerForRestaurant = (restaurant) => {
+    let latLng = new google.maps.LatLng(restaurant.lat, restaurant.lng);
+    let marker = new google.maps.Marker({
+      position: latLng,
+      map: googleMap.map,
+      icon: "/images/marker.png"
+    });
+    googleMap.addInfoWindow(restaurant, marker);
+  };
+
+
+  googleMap.loopThroughRestaurants = (data) => {
+    $.each(data.restaurants, (index, restaurant) => {
+      googleMap.createMarkerForRestaurant(restaurant);
+    });
+  };
+
+
+  $(googleMap.mapSetup.bind(googleMap));
+
+
+
+
+  document.getElementById("locationButton").addEventListener("click", function(){
+    navigator.geolocation.getCurrentPosition((position) => {
+      let latLng = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      googleMap.map.panTo(latLng);
+      googleMap.map.setZoom(14);
+      let marker = new google.maps.Marker({
+        position: latLng,
+        animation: google.maps.Animation.DROP,
+        draggable: true,
+        map: googleMap.map
+      });
+    });
   });
-  googleMap.addInfoWindow(restaurant, marker);
-};
 
-
-googleMap.loopThroughRestaurants = (data) => {
-  $.each(data.restaurants, (index, restaurant) => {
-    googleMap.createMarkerForRestaurant(restaurant);
-  });
-};
-
-
-$(googleMap.mapSetup.bind(googleMap));
+});
