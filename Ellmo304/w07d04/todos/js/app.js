5936c0ec73e04fd765300d694f9b309cea02c3d2
Elliot Brock <ell_304@msn.com>
2016-11-10 19:51:11 +0000

    finished walkthrough

---
+++ Ellmo304/w07d04/todos/js/app.js
@@ -0,0 +1,23 @@
+angular
+	.module('todoApp', ['ui.router'])
+  .config(MainRouter);
+
+
+MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'home.html'
+  })
+  .state('archive', {
+    url: '/archive',
+    templateUrl: 'archive.html'
+  })
+  .state('edit', {
+    url: '/edit',
+    templateUrl: 'edit.html'
+  });
+  $urlRouterProvider.otherwise('/');
+}
