675d285029c73cbb6fae613454c2de7f8b37325b
Elliot Brock <ell_304@msn.com>
2016-12-01 16:19:51 +0000

    added sessions controller

--- Ellmo304/w10d04/comics_app/config/routes.rb
+++ Ellmo304/w10d04/comics_app/config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  get 'users/create'
+  root 'comics#index'
+
+  get '/register', to: 'users#new'
+
+  post '/register', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 
   resources :comics
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
