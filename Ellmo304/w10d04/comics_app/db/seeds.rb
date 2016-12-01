d03da0cc1202de7a71a5b8ae2652970e9c28af8b
Elliot Brock <ell_304@msn.com>
2016-12-01 19:18:04 +0000

    finished styling

--- Ellmo304/w10d04/comics_app/db/seeds.rb
+++ Ellmo304/w10d04/comics_app/db/seeds.rb
@@ -11,20 +11,20 @@ ActiveRecord::Base.connection.execute("TRUNCATE comics RESTART IDENTITY")
 Comic.create!([{
 	title: "The Invisibles",
 	author: "Grant Morrison",
-	image: "",
-	issue: 1,
-
-},
-{
-  title: "Detective Comics",
-  author: "Scott Snyder",
-  image: "",
-  issue: 100,
+	image: "http://www.readcomics.tv/images/manga/invisibles-the/23/1.jpg",
+	issue: 23,
 
 },
 {
   title: "Saga",
-	author: "Fiona Staples",
-	image: "",
+	author: "Brian K Vaughn",
+	image: "http://static8.comicvine.com/uploads/scale_large/6/67663/3501436-05.jpg",
 	issue: 5,
+},
+{
+  title: "Detective Comics",
+  author: "Scott Snyder",
+  image: "http://static1.comicvine.com/uploads/scale_large/5/56044/2380309-detective_comics__1937___880.jpeg",
+  issue: 880,
+
 }])
